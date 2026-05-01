e45e350eb2ab04b54853afeeaab5484b787ab714
sprawl <sprawl@onlajn.nu>
2026-05-01 20:41:08 +0200

    feat: add @ghost/sentinel-redemeine package for command authorization via Sentinel

---
+++ packages/sentinel-redemeine/src/index.ts
@@ -0,0 +1,4 @@
+export { createSentinelPlugin } from './sentinel-plugin.js';
+export { resolveAction } from './action-mapper.js';
+export { AuthorizationError } from './types.js';
+export type { SentinelPluginConfig, PrincipalResolver, EvaluationMode, ActionMap, SentinelStoreReader } from './types.js';
